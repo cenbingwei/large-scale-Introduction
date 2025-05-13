496c7afee2bb26001eba7845fe2f37af91d87979
Jiang <2280130437@qq.com>
2025-05-13 11:09:05 +0800

    添加了selectStudentGradesWithCourseInfo（返回指定学生的所有课程成绩信息）和selectClassAverageAndTopStudent（获取每个班级的最高分学生）两个拥有JOIN的复杂查询

--- largescale/src/main/java/com/waxjx/largescale/service/StudentService.java
+++ largescale/src/main/java/com/waxjx/largescale/service/StudentService.java
@@ -17,7 +17,7 @@ import java.util.Map;
 import java.util.Date;
 
 @Service
-public class StudentService  {
+public class    StudentService  {
 
     @Autowired
     private StudentMapper studentMapper;
